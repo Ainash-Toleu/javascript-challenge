6af2fda826520f9edfc7d340588c24a6424ebb24
Ainash Toleu <ainashtoleu@Ainashs-MacBook-Air.local>
2021-04-13 13:34:42 -0500

    event to the handler function attached

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,5 +1,37 @@
-// from data.js
+// checking connections
 console.log ("app.js loaded");
+console.log ("data.js loaded");
+console.log(data);
+
+// geting a reference to the table body
+var tbody = d3.select("tbody");
+
+// from data.js
 var tableData = data;
 
 // YOUR CODE HERE!
+// append all data in table
+data.forEach((ufoReport) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoReport).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+
+// Getting a reference to the button on the page with the id property set to `filter-btn`
+var button = d3.select("#filter-btn");
+
+// Getting a reference to the input element on the page with the id property set to 'datetime'
+var inputField = d3.select("#datetime");
+
+// This function is triggered when the button is clicked
+function handleClick() {
+  console.log("A button was clicked!");
+
+  // We can use d3 to see the object that dispatched the event
+  console.log(d3.event.target);
+}
+
+// We can use the `on` function in d3 to attach an event to the handler function
+button.on("click", handleClick);  
