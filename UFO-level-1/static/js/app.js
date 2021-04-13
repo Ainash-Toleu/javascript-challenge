45341fa4198d18423a555eee32c50259c10405d9
Ainash Toleu <ainashtoleu@Ainashs-MacBook-Air.local>
2021-04-13 15:10:31 -0500

    Initial version of filtering data

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -19,34 +19,88 @@ data.forEach((ufoReport) => {
     });
   });
 
-// Getting a reference to the button on the page with the id property set to `filter-btn`
+// // Getting a reference to the button on the page with the id property set to `filter-btn`
+// var button = d3.select("#filter-btn");
+
+// // Getting a reference to the input element on the page with the id property set to 'datetime'
+// var inputField = d3.select("#datetime");
+
+// // This function is triggered when the button is clicked
+// function handleClick() {
+//   console.log("A button was clicked!");
+
+//   // We can use d3 to see the object that dispatched the event
+//   console.log(d3.event.target);
+// }
+
+// // We can use the `on` function in d3 to attach an event to the handler function
+// button.on("click", handleClick);  
+
+// // grab references to the input element and the output div
+// var text = d3.select("#datetime");
+// var output = d3.select(".output");
+
+// // Function to handle input change
+// function handleChange(event) {
+//   // grab the value of the input field
+//   var inputText = d3.event.target.value;
+
+//   // Set the output text 
+//   output.text(inputText);
+// }
+
+// text.on("change", handleChange);
+
+// Select the button
 var button = d3.select("#filter-btn");
 
-// Getting a reference to the input element on the page with the id property set to 'datetime'
-var inputField = d3.select("#datetime");
+// Select the form
+var form = d3.select("#form");
 
-// This function is triggered when the button is clicked
-function handleClick() {
-  console.log("A button was clicked!");
+// Create event handlers 
+button.on("click", runEnter);
+form.on("submit",runEnter);
 
-  // We can use d3 to see the object that dispatched the event
-  console.log(d3.event.target);
-}
+// Complete the event handler function for the form
+function runEnter() {
 
-// We can use the `on` function in d3 to attach an event to the handler function
-button.on("click", handleClick);  
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+  
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
 
-// grab references to the input element and the output div
-var text = d3.select("#datetime");
-var output = d3.select(".output");
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
 
-// Function to handle input change
-function handleChange(event) {
-  // grab the value of the input field
-  var inputText = d3.event.target.value;
+  console.log(inputValue);
+  console.log(tableData);
 
-  // Set the output text to the reversed input string
-  output.text(inputText);
-}
+  var filteredData = tableData.filter(ufo => ufo.datetime === inputValue);
 
-text.on("change", handleChange);
+  console.log("Filtered Data: ");
+  console.log(filteredData);
+
+  var dateTime = filteredData.map(ufo => ufo.datetime);
+  var city = filteredData.map(ufo => ufo.city);
+  var state = filteredData.map(ufo => ufo.state);
+  var country = filteredData.map(ufo => ufo.country);
+  var shape = filteredData.map(ufo => ufo.shape);
+  var minutes = filteredData.map(ufo => ufo.durationMinutes);
+  var comments = filteredData.map(ufo => ufo.comments);
+
+  // Then, select the unordered list element by class name
+  var list = d3.select(".summary");
+
+  // remove any data from the list to
+  list.html("");
+
+  // append stats to the list
+  list.append("li").text(`Datetime: ${dateTime}`);
+  list.append("li").text(`City: ${city}`);
+  list.append("li").text(`State: ${state}`);
+  list.append("li").text(`Country: ${country}`);
+  list.append("li").text(`Shape: ${shape}`);
+  list.append("li").text(`Duration minutes: ${minutes}`);
+  list.append("li").text(`Comments: ${comments}`);
+};
