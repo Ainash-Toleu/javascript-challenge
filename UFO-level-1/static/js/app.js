54c29faeb9f8197d8f2fe964d8cb99e6540167b9
Ainash Toleu <ainashtoleu@Ainashs-MacBook-Air.local>
2021-04-17 11:43:31 -0500

    code for adding filtered data in table added

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -19,38 +19,6 @@ data.forEach((ufoReport) => {
     });
   });
 
-// // Getting a reference to the button on the page with the id property set to `filter-btn`
-// var button = d3.select("#filter-btn");
-
-// // Getting a reference to the input element on the page with the id property set to 'datetime'
-// var inputField = d3.select("#datetime");
-
-// // This function is triggered when the button is clicked
-// function handleClick() {
-//   console.log("A button was clicked!");
-
-//   // We can use d3 to see the object that dispatched the event
-//   console.log(d3.event.target);
-// }
-
-// // We can use the `on` function in d3 to attach an event to the handler function
-// button.on("click", handleClick);  
-
-// // grab references to the input element and the output div
-// var text = d3.select("#datetime");
-// var output = d3.select(".output");
-
-// // Function to handle input change
-// function handleChange(event) {
-//   // grab the value of the input field
-//   var inputText = d3.event.target.value;
-
-//   // Set the output text 
-//   output.text(inputText);
-// }
-
-// text.on("change", handleChange);
-
 // Select the button
 var button = d3.select("#filter-btn");
 
@@ -81,26 +49,36 @@ function runEnter() {
   console.log("Filtered Data: ");
   console.log(filteredData);
 
-  var dateTime = filteredData.map(ufo => ufo.datetime);
-  var city = filteredData.map(ufo => ufo.city);
-  var state = filteredData.map(ufo => ufo.state);
-  var country = filteredData.map(ufo => ufo.country);
-  var shape = filteredData.map(ufo => ufo.shape);
-  var minutes = filteredData.map(ufo => ufo.durationMinutes);
-  var comments = filteredData.map(ufo => ufo.comments);
+  tbody.html("");
+
+  filteredData.forEach((ufoSelected) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoSelected).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+
+  // var dateTime = filteredData.map(ufo => ufo.datetime);
+  // var city = filteredData.map(ufo => ufo.city);
+  // var state = filteredData.map(ufo => ufo.state);
+  // var country = filteredData.map(ufo => ufo.country);
+  // var shape = filteredData.map(ufo => ufo.shape);
+  // var minutes = filteredData.map(ufo => ufo.durationMinutes);
+  // var comments = filteredData.map(ufo => ufo.comments);
 
   // Then, select the unordered list element by class name
-  var list = d3.select(".summary");
+  // var list = d3.select(".summary");
 
-  // remove any data from the list to
-  list.html("");
+  // // remove any data from the list to
+  // list.html("");
 
-  // append stats to the list
-  list.append("li").text(`Datetime: ${dateTime}`);
-  list.append("li").text(`City: ${city}`);
-  list.append("li").text(`State: ${state}`);
-  list.append("li").text(`Country: ${country}`);
-  list.append("li").text(`Shape: ${shape}`);
-  list.append("li").text(`Duration minutes: ${minutes}`);
-  list.append("li").text(`Comments: ${comments}`);
+  // // append stats to the list
+  // list.append("li").text(`Datetime: ${dateTime}`);
+  // list.append("li").text(`City: ${city}`);
+  // list.append("li").text(`State: ${state}`);
+  // list.append("li").text(`Country: ${country}`);
+  // list.append("li").text(`Shape: ${shape}`);
+  // list.append("li").text(`Duration minutes: ${minutes}`);
+  // list.append("li").text(`Comments: ${comments}`);
 };
